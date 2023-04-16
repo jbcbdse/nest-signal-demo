3d29ee44153896dffc58dacab091a231c494c49e
Jonathan Barnett <jonbarnett@gmail.com>
2023-04-16 19:30:30 +0000

    Refactor business logic into a method

--- src/poll.service.ts
+++ src/poll.service.ts
@@ -6,15 +6,18 @@ export class PollService {
   private logger = new Logger('PollService')
   constructor(private tx: TransactionTracker) { }
   async poll() {
-    const secondsToWait = 10;
     while (true) {
       const done = this.tx.startTransaction();
       if (!done) { break; }
-      this.logger.debug(`Poller will wait for ${secondsToWait} seconds`);
-      await new Promise((resolve) => setTimeout(resolve, secondsToWait * 1000));
-      this.logger.debug(`Poll complete`);
+      await this.performTask()
       done();
     }
     this.logger.debug(`Poller is finished`);
+  }
+  private async performTask() {
+    const secondsToWait = 10;
+    this.logger.debug(`Poller will wait for ${secondsToWait} seconds`);
+    await new Promise((resolve) => setTimeout(resolve, secondsToWait * 1000));
+    this.logger.debug(`Poll complete`);
   }
 }
